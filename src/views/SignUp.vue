5e27203c487c49090cafc5f331b72256267e454c
vitamin52 <1006293043@qq.com>
2021-04-03 12:52:39 +0800

    day 20210403

---
+++ src/views/SignUp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id = "sign-up-box">
+    注册账号
+  </div>
+</template>
+
+<script>
+export default {
+name: "SignUp"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/forgotPassword.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id ="forgot-box">
+      忘记密码
+  </div>
+</template>
+
+<script>
+export default {
+name: "forgotPassword"
+}
+</script>
+
+<style scoped>
+
+</style>
